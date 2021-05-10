3123eb799d2b97aa711aab8073e4b911ab6ccef5
LAnqxpp <azir_likes@126.com>
2021-05-10 10:50:16 +0800

    哎

--- XTTDemo/AppDelegate.m
+++ XTTDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //1.0bug修复
     return YES;
 }
 
